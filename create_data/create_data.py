b58b069bff0b0a9727669ebcd6b535216151b279
Michael Lellouch <mlellouch@aligntech.com>
2021-09-15 02:25:19 +0300

    Finished data creator

--- create_data/create_data.py
+++ create_data/create_data.py
@@ -7,9 +7,11 @@ import tempfile
 import pathlib
 import uuid
 import video_database
+import matplotlib.pyplot as plt
 
 def detect_faces(img):
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    # face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     return faces
@@ -21,6 +23,7 @@ class UnexistingStreamException(Exception):
 class DataCreator:
     """
     Downloads videos, filters them, and writes the output frames to the data
+    :return the fps of the downloaded video
     """
 
     def _download_video(self, link: str, target_dir: str, filename: str):
@@ -39,12 +42,60 @@ class DataCreator:
         if stream_to_download is None:
             raise UnexistingStreamException(link)
 
+        video_fps = stream_to_download.fps
         stream_to_download.download(output_path=target_dir, filename=filename)
+        return video_fps
 
-    def _filter_video(self, source_video, target_dir):
+    def _validate_faces(self, frame):
+        """
+        Check that image contains a single face, and if so returns its center
+        :param frame:
+        :return: int, tuple(int, int) - # number of faces detected, and if a single face was detected its center
+        """
+
+        faces = detect_faces(frame)
+        faces_detected = len(faces)
+        if faces_detected != 1:
+            return faces_detected, None
+
+        x, y, w, h = faces[0]
+        return 1, (x + w/2, y + h/2)
+
+    def _process_frame(self, frame):
+        faces_detected, center = self._validate_faces(frame)
+        if faces_detected != 1:
+            return False, None
+
+        # crop around face center
+        def get_best_start_finish(center, image_size, crop_size):
+            if center < crop_size/2:
+                return 0, crop_size
+
+            if image_size - center < crop_size/2:
+                return image_size-crop_size, image_size
+
+            return int(center - crop_size/2), int(center + crop_size/2)
+
+        y = get_best_start_finish(center[1], frame.shape[0], self._resolution[1])
+        x = get_best_start_finish(center[0], frame.shape[1], self._resolution[0])
+
+        final_frame = frame[y[0]:y[1], x[0]:x[1]]
+        return True, final_frame
+
+
+    def _filter_video(self, source_video, target_dir, fps):
         cap = cv2.VideoCapture(source_video)
-        ret, frame = cap.read()
-        cv2.imwrite(os.path.join(target_dir, "0.png"), frame)
+        number_of_frames = 0
+        while number_of_frames < self._max_frames_per_video and cap.isOpened():
+            ret, frame = cap.read()
+            success, final_frame = self._process_frame(frame)
+            if success:
+                cv2.imwrite(os.path.join(target_dir, f"{number_of_frames}.png"), final_frame)
+                number_of_frames += 1
+
+            for i in range(int(fps * self._frame_skip)):
+                ret, frame = cap.read()
+
         cap.release()
 
     def _process_videos(self, links, output_dir):
@@ -52,18 +103,21 @@ class DataCreator:
             for index, link in enumerate(links):
                 filename = f'{uuid.uuid4()}.mp4'
                 try:
-                    self._download_video(link, target_dir=temp_dir, filename=filename)
+                    video_fps = self._download_video(link, target_dir=temp_dir, filename=filename)
                 except UnexistingStreamException:
                     continue
                 video_out_dir = os.path.join(output_dir, str(index))
-                os.makedirs(video_out_dir, exist_ok=False)
-                self._filter_video(os.path.join(temp_dir, filename), video_out_dir)
+                os.makedirs(video_out_dir, exist_ok=True)
+                try:
+                    self._filter_video(os.path.join(temp_dir, filename), video_out_dir, video_fps)
+                except cv2.error:
+                    continue
 
 
-    def __init__(self, frame_skip: int, resolution: Tuple[int, int], max_frames_per_video: int, download_resolution='480p', zoom_on_face=True):
+    def __init__(self, frame_skip: float, resolution: Tuple[int, int], max_frames_per_video: int, download_resolution='480p', zoom_on_face=True):
         """
-        :param frame_skip: by how much should we skip between frames. Used to make sure
-               that the data doesn't contain frames that are similar
+        :param frame_skip: by how much should we skip between frames, in seconds. Used to make sure
+               that the data doesn't contain frames that are similar.
         :param resolution: the resolution of the output
         :param max_frames_per_video: the maximum number of frames that are outputted
                per video. Used to make sure our database isn't to big
@@ -109,5 +163,5 @@ class DataCreator:
 
 
 if __name__ == '__main__':
-    dc = DataCreator(frame_skip=1, resolution=(480, 480), max_frames_per_video=300)
+    dc = DataCreator(frame_skip=3, resolution=(336, 336), max_frames_per_video=150)
     dc.create_data(video_database.videos)
